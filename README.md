595903079374c0ce0f191f118868dec152c25f1c
qiyi <bphanzhu@gmail.com>
2017-11-26 21:06:18 +0800

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# platinum
+# retrofit-form
+
+retrofit-form 支持使用 retrofit 发起 form data 请求时，使用自定义的对象作为参数。使用方式如下:
+
+    public interface AccountService {
+    
+        @POST("/accounts")
+        Call<AccountInfo> addAccountInfo(@Body @FormBody AccountInfo accountInfo);
+    }
+    
+即将 form data 的参数定义到一个复杂对象里，在接口定义上添加 ``@FormBody`` 注解.
+ 
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("http://127.0.0.1:8080/")
+            .addConverterFactory(new FormBodyConverterFactory())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+    AccountService accountService = retrofit.create(AccountService.class);
+    AccountInfo accountInfo = accountService.addAccountInfo(
+            new AccountInfo()
+                    .setEmail("bphanzhu@live.com")
+                    .setBio("love 7777"))
+            .execute()
+            .body();
+
+然后创建 Retrofit 对象时，添加 FormBodyConverterFactory.
+
+
---
+++ src/main/java/org/isouth/platinum/FormBodyConverterFactory.java
@@ -0,0 +1,61 @@
+package org.isouth.platinum;
+
+import okhttp3.RequestBody;
+import retrofit2.Converter;
+import retrofit2.Retrofit;
+
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FormBodyConverterFactory extends Converter.Factory {
+    public Converter<?, RequestBody> requestBodyConverter(
+            Type type, Annotation[] parameterAnnotations,
+            Annotation[] methodAnnotations, Retrofit retrofit) {
+        return type instanceof Class<?> ? Arrays.stream(parameterAnnotations)
+                .filter(a -> a instanceof FormBody)
+                .findAny()
+                .map(a -> new FormBodyConverter<>(retrofit, (Class<?>) type))
+                .orElse(null) : null;
+    }
+
+    public class FormBodyConverter<T> implements Converter<T, RequestBody> {
+        private final Retrofit retrofit;
+        private final Field[] fields;
+        private final Map<String, String> fieldKeys = new HashMap<>();
+
+        public FormBodyConverter(Retrofit retrofit, Class<?> type) {
+            this.retrofit = retrofit;
+            this.fields = type.getDeclaredFields();
+            for (Field field : this.fields) {
+                field.setAccessible(true);
+                FormField formField = field.getAnnotation(FormField.class);
+                if (formField != null) {
+                    fieldKeys.put(field.getName(), formField.value());
+                }
+            }
+        }
+
+        @Override
+        public RequestBody convert(T value) throws IOException {
+            okhttp3.FormBody.Builder builder = new okhttp3.FormBody.Builder();
+            for (Field field : fields) {
+                Object fieldValue;
+                try {
+                    fieldValue = field.get(value);
+                } catch (IllegalAccessException e) {
+                    throw new IOException("Get field value failed.", e);
+                }
+                if (fieldValue != null) {
+                    String fieldKey = fieldKeys.getOrDefault(field.getName(), field.getName());
+                    builder.add(fieldKey, String.valueOf(fieldValue));
+                }
+            }
+            return builder.build();
+        }
+    }
+}
